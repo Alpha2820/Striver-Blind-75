ffc73d9d7abea5a589bcf51b5ea0c0149f5404e1
Abhijeet Tiwari <abhijeett709@gmail.com>
2025-11-22 15:58:09 +0530

    Product of Array Except Itself

---
+++ Day_1_Array/ProductOfArrayExceptItself.java
@@ -0,0 +1,33 @@
+package Day_1_Array;
+
+import java.util.*;
+
+public class ProductOfArrayExceptItself {
+  public static int[] productOfArray(int nums[]) {
+    int n = nums.length;
+    int result[] = new int[n];
+    int prefix = 1;
+    for (int i = 0; i < nums.length; i++) {
+      result[i] = prefix;
+      prefix = prefix * nums[i];
+    }
+    int postfix = 1;
+    for (int i = n - 1; i >= 0; i--) {
+      result[i] = result[i] * postfix;
+      postfix = postfix * nums[i];
+    }
+    return result;
+  }
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int nums[] = new int[n];
+    for (int i = 0; i < n; i++) {
+      nums[i] = sc.nextInt();
+    }
+    int result[] = productOfArray(nums);
+    System.out.println(Arrays.toString(result));
+    sc.close();
+  }
+}
