bf93293ee4edaf72e1a642497020464fa08cac45
Abhijeet Tiwari <abhijeett709@gmail.com>
2025-11-23 17:05:58 +0530

    Container With maximum water is solved

--- Day_1_Array/containerWithMostWater.java
+++ Day_1_Array/containerWithMostWater.java
@@ -23,13 +23,15 @@ public class containerWithMostWater {
 
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the elements of the array:");
     int n = sc.nextInt();
     int nums[] = new int[n];
+    System.out.println("Enter the elements of the array:");
     for (int i = 0; i < n; i++) {
       nums[i] = sc.nextInt();
     }
     int result = maxWater(nums);
-    System.out.println(result);
+    System.out.println("The maximum water that can be contained is: " + result);
     sc.close();
   }
 }
