45efbe6c8c527395d4259301adb2239618c1846d
Abhijeet Tiwari <abhijeett709@gmail.com>
2025-11-23 17:08:41 +0530

    Product of Array except itself problem solved

--- Day_1_Array/ProductOfArrayExceptItself.java
+++ Day_1_Array/ProductOfArrayExceptItself.java
@@ -21,13 +21,15 @@ public class ProductOfArrayExceptItself {
 
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the number of elements in the array:");
     int n = sc.nextInt();
     int nums[] = new int[n];
+    System.out.println("Enter the elements of the array:");
     for (int i = 0; i < n; i++) {
       nums[i] = sc.nextInt();
     }
     int result[] = productOfArray(nums);
-    System.out.println(Arrays.toString(result));
+    System.out.println("The product array is:" + Arrays.toString(result));
     sc.close();
   }
 }
