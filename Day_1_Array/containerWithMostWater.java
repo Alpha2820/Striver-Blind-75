c64cc85710d45debc5335e16238a3fbecd2bb527
Abhijeet Tiwari <abhijeett709@gmail.com>
2025-11-23 16:27:26 +0530

    Container With Most Water Problem Solved

---
+++ Day_1_Array/containerWithMostWater.java
@@ -0,0 +1,35 @@
+package Day_1_Array;
+
+import java.util.*;
+
+public class containerWithMostWater {
+  public static int maxWater(int nums[]) {
+    int left = 0;
+    int right = nums.length - 1;
+    int maxArea = 0;
+    while (left <= right) {
+      int max = (right - left) * Math.min(nums[left], nums[right]);
+      maxArea = Math.max(maxArea, max);
+      if (nums[left] < nums[right]) {
+        left++;
+      } else if (nums[left] > nums[right]) {
+        right--;
+      } else {
+        left++;
+      }
+    }
+    return maxArea;
+  }
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int nums[] = new int[n];
+    for (int i = 0; i < n; i++) {
+      nums[i] = sc.nextInt();
+    }
+    int result = maxWater(nums);
+    System.out.println(result);
+    sc.close();
+  }
+}
